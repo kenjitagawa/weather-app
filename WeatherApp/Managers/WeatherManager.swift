bf8c8fdf19650aa2ba70b01d89a6cca276c6a0a7
Kenji Tagawa <kenji@tagawa.ca>
2024-01-14 16:43:25 -0500

    feat: init project

---
+++ WeatherApp/Managers/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  WeatherApp
+//
+//  Created by Kenji Tagawa on 2024-01-14.
+//
+
+import Foundation
---
+++ WeatherApp/Preview Content/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  WeatherApp
+//
+//  Created by Kenji Tagawa on 2024-01-14.
+//
+
+import Foundation
