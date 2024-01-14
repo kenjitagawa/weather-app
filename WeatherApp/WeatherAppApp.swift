fb20bbb5c726b2359d5319faf519347134cc512c
Kenji Tagawa <kenji@tagawa.ca>
2024-01-14 12:16:02 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Kenji Tagawa on 2024-01-14.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
